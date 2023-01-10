89ab53012120f6cd482c36d2ca234a6d0e1a13ef
Sneha Purkayastha <iamsneha0296@gmail.com>
2023-01-10 18:40:50 +0530

    new commit

--- script.js
+++ script.js
@@ -1,9 +1,2 @@
-const addText = document.getElementById('para');
-function newForm(){
-    var text = document.forms["form1"]["personText"].value;
-    if(text === ''){
-        console.log("clicked")
-        addText.textContent = "something";
-        addText.style.color = 'red';
-    }
-}
+
+
